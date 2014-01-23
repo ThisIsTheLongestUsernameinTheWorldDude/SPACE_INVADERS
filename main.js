cf9d0da206303f53b252c919d49a8749892bef63
ThisIsTheLongestUsernameinTheWorldDude <cpwest11@gmail.com>
2014-01-22 18:01:03 -0600

    spawnEnemy
    Created a function to generate an enemy with a x value, y value, and
    type determined via input into constructor function spawnEnemy

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 enchant();
+
 window.onload = function(){
-	var game = new Core(400, 400);
-	game.fps = 15;
-	game.preload("invaders.png");
-	game.onload = function(){
-		var invader = new Sprite(141, 217);
-		invader.image = game.assets["invaders.png"];
-		invader.x = 0;
-		invader.y = 0;
-		invader.frame = 1;
-		game.rootScene.addChild(invader);
-		
-		invader.addEventListener("enterframe", function(){
-			this.frame = this.age % 2;
-		})
-	}
-}
+    var game = new Core(320, 320);
+    game.fps = 15;
+    game.preload("invaders.png");
+	function spawnEnemy(startx, starty, enemytype){
+		var invader = new Sprite(32, 32);
+			invader.image = game.assets["invaders.png"];
+			invader.x = startx;
+			invader.y = starty;
+			invader.frame = 5;
+			game.rootScene.addChild(invader);
+
+			invader.addEventListener("enterframe", function(){
+				this.x += 1;
+				this.frame = this.age % 2 + 6;
+			});
+
+			invader.addEventListener("touchstart", function(){
+				game.rootScene.removeChild(invader);
+			});
+		return invader;
+		}
+    game.onload = function(){
+		var enemy1 = spawnEnemy(0, 0, 0);
+		var enemy2 = spawnEnemy(50, 50, 0);
+    };
+    game.start();
+};
