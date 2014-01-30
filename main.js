8b12f8ccca2afb72b743caf30c90a7c66f6d5ae6
ThisIsTheLongestUsernameinTheWorldDude <cpwest11@gmail.com>
2014-01-30 13:02:40 -0600

    umm
    bug

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ window.onload = function(){
     var game = new Core(320, 320);
 	game.framedelay = 4;
     game.fps = 15;
+	game.enemydirection = 1;
+	// 1 means move right, -1 means move left
+	game.enemyonedge = false;
+	game.addEventListener("enterframe", function(){
+		if(this.enemyonedge){
+			this.enemydirection = this.enemydirection * -1;
+			this.enemyonedge = false;
+		}else{
+			this.enemydirection = this.enemydirecton;
+		}
+	});
     game.preload("invaders.png");
 	function spawnEnemy(startx, starty, enemytype){
 		var invader = new Sprite(31, 27);
@@ -17,11 +28,18 @@ window.onload = function(){
 				}else{
 				this.frame = this.frame - 1;
 				}
-				this.x += 3;
+				this.x += 3 * game.enemydirection;
 			}
 			game.rootScene.addChild(invader);
 
 			invader.addEventListener("enterframe", function(){
+				if((this.x == 320 - (31-5)) && (game.enemydirection == 1)){
+					game.enemyonedge = true;
+				}else if((this.x == (31-4)) && (game.enemydirection == -1)){
+					game.enemyonedge = true;
+				}else{
+					game.enemyonedge = false;
+				}
 				if((this.age % game.framedelay) == 0){
 					this.newFrame();
 				}else{
